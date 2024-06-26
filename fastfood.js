4d7c79d67da9dca1235a73e5839d5b6932cd23b5
Tannusaini6003 <sainitanu6003@gmail.com>
2024-06-26 14:53:34 +0530

    first commit

---
+++ fastfood.js
@@ -0,0 +1,11 @@
+//  function initMap(){
+//     const map = document.getElementById('map') {
+//         zoom: 10,  // Zoom level
+//         center: location
+//     });
+
+//     // Add a click event listener to the map
+//     map.addListener("click", (event) => {
+//         alert("You clicked on the map at latitude: " + event.latLng.lat() + ", longitude: " + event.latLng.lng());
+//     });
+//  }
